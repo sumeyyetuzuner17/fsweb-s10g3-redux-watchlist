5276c0b4b49d69ffd4b461da4a3190b49925f74c
sumeyyetuzuner17 <sumeyyedeniz34@gmail.com>
2023-02-04 13:33:35 +0300

    devam edilcek

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { Provider } from 'react-redux';
 import { BrowserRouter } from "react-router-dom"
+import {legacy_createStore as createStore} from "redux"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store=createStore(reducer)
+
 root.render(
+  <Provider store={store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </Provider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
